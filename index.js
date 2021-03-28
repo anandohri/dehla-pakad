5551b065e29d03f7a536deb9a15957b5f526f125
Anand Ohri <78968462+anandohri@users.noreply.github.com>
2021-03-29 02:49:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class DehlaPakad extends React.Component{
+
+}
+
+ReactDOM.render(
+  <DehlaPakad />,
+  document.getElementById('root')
+);

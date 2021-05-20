49092cf3269b070eab9c8767a7a892ef8ee805f5
Anand Ohri <78968462+anandohri@users.noreply.github.com>
2021-05-21 00:43:03 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -9,6 +9,20 @@ console.log('listening to port 8000');
 const players = {};
 let id = 1;
 
+const deck = ['1 S','2 S','3 S','4 S','5 S','6 S','7 S','8 S','9 S','10 S','11 S','12 S','13 S',
+                '1 H','2 H','3 H','4 H','5 H','6 H','7 H','8 H','9 H','10 H','11 H','12 H','13 H',
+                '1 C','2 C','3 C','4 C','5 C','6 C','7 C','8 C','9 C','10 C','11 C','12 C','13 C',
+                '1 D','2 D','3 D','4 D','5 D','6 D','7 D','8 D','9 D','10 D','11 D','12 D','13 D'];
+
+const shuffle = () => {
+    for(let i = 51; i > 0; --i){
+        const j = Math.floor(Math.random() * (i+1));
+        const temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+
 const wsServer = new webSocketServer({
     httpServer: server
 });
@@ -20,6 +34,9 @@ wsServer.on('request', function(request){
             const move = JSON.parse(message.utf8Data)
             if(move.type === 'login'){
                 if(id <= 4){
+                    if(id === 1){
+                        shuffle();
+                    }
                     move['id'] = id;
                     players[id] = connection;
                     console.log(move, 'connected as player', id)
